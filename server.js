c09f58ee391a68aa6a6e3c5b47cbd7bfd0a3b6b1
vasu sridharan <vasusridharan1@gmail.com>
2017-08-23 14:16:23 +1000

    Node js web server Initial commit

---
+++ server.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', hbs);
+
+//Logger middleware - logs requests in a file
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = now + " " + req.url + " " + req.method;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) throw err;
+    console.log('Saved!');
+  });
+  next(); //important!! without this, the routes will not work
+});
+
+//maintenance middleware
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+//   //note the missing next() which means user cannot navigate to any page.
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (request, response) => {
+  response.render('home.hbs', {
+    pageTitle: 'Home Page',
+    name: "Vasu"
+  });
+});
+
+app.get('/about', (request, response) => {
+  response.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server is up listening on port:3000");
+});
+
+app.get('/bad', (request, response) => {
+  response.send({
+    'error': 'bad request provided'
+  })
+});
