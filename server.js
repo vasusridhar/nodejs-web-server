6b1b8721a950a9a26d4b5ea67bac87c86f92d743
vasu sridharan <vasusridharan1@gmail.com>
2017-08-23 18:19:27 +1000

    added port for heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 3000;
 
 var app = express();
 
@@ -48,8 +49,8 @@ app.get('/about', (request, response) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("Server is up listening on port:3000");
+app.listen(port, () => {
+  console.log(`Server is up listening on port: ${port}`);
 });
 
 app.get('/bad', (request, response) => {
